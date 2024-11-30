d5162285c8d896e22128e8db5d926fafa0f62c5f
smakon <25469@inbox.ru>
2024-11-30 13:44:01 +0300

    [L]

--- server.js
+++ server.js
@@ -5,22 +5,38 @@ const mysql = require('mysql')
 const cors = require('cors')
 const bcrypt = require('bcryptjs')
 
-
+// Cloud DB
 const conn = mysql.createConnection({
-	host: 'jufirjigoop.beget.app:3306',
+	host: 'jufirjigoop.beget.app',
 	user: 'dnd_player_builder',
 	password: '12345678Sas)',
 	database: 'dnd_player_builder',
 })
+// setInterval(() => {
+// 	conn.query('select 1')
+// 	console.log('select')
+// }, 2900)
+
+// DB
+// const conn = mysql.createConnection({
+// 	host: 'ssamikm6.beget.tech',
+// 	user: 'ssamikm6_dnd',
+// 	password: '12345678Sas)',
+// 	database: 'ssamikm6_dnd',
+// })
 
 conn.connect(err => {
 	if (err) {
 		console.log(err)
 		return err
 	} else {
+		conn.query('set session wait_timeout=28800;')
 		console.log('Connected to MySQL')
 	}
 })
+
+conn.wa
+
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -46,12 +62,10 @@ app.post('/findUser/:name/:password', async (req, res) => {
 		} else if (result.length == 0) {
 			res.send('No user found')
 		} else {
-
 			const password = bcrypt.compareSync(
 				req.params.password,
 				result[0].password
 			)
-			console.log(password)
 
 			if (password == true) {
 				res.send(result)
@@ -71,14 +85,18 @@ app.post('/createUser/:name/:password', async (req, requestResult) => {
 		let resArr = []
 		resArr.push(result)
 		if (resArr[0].length == 0) {
-			conn.query(sql, [req.params.name, password],(err, res) => {
+			conn.query(sql, [req.params.name, password], (err, res) => {
 				if (err) {
 					throw err
 				} else {
-					conn.query(sql, (err, result) => {
-						console.log(result)
+					conn.query(isUserSqlReq, (err, result) => {
+						conn.query(
+							`INSERT INTO user_data (user_id) VALUES (${result[0].id})`,
+							(err, result) => {
+								requestResult.send(true)
+							}
+						)
 					})
-					requestResult.send(true)
 				}
 			})
 		} else {
@@ -113,24 +131,24 @@ app.post('/getUserData/:id', async (req, res) => {
 		if (err) {
 			console.log(err)
 		}
-		console.log(result)
-
 		res.send(result)
 	})
 })
 
-app.post('/updateUserData/:id/:dice/:theme/:vibration/:language',async (req, res) => {
-		let sql = `UPDATE user_data SET theme = "${req.params.theme}", vibration = "${req.params.vibration}", language = "${req.params.language}", WHERE user_id = ${req.params.id}`
-      conn.query(sql, (err, result) => {
-         if (err) {
-            console.log(err)
-         }
-         res.send(result)
-      })
+app.post(
+	'/updateUserData/:id/:dice/:theme/:vibration/:language',
+	async (req, res) => {
+		let sql = `UPDATE user_data SET theme = "${req.params.theme}", vibration = "${req.params.vibration}", language = "${req.params.language}", dice_count = "${req.params.dice}" WHERE user_id = ${req.params.id}`
+		conn.query(sql, (err, result) => {
+			if (err) {
+				console.log(err)
+			}
+			res.send(result)
+		})
 	}
 )
+
 const PORT = 2205
-app.listen(PORT, () => {
-	console.debug(`listening on port: http://localhost:${PORT}`)
-	console.debug(`listening on Network: http://192.168.1.249:${PORT}`)
+app.listen(PORT, (err, result) => {
+	console.debug('listening on port %d', PORT)
 })
